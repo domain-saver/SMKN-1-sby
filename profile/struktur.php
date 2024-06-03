71aa901237587f596c06cce5952405ccd07a58fb
Risky <131745729+RyuXyro@users.noreply.github.com>
2024-06-03 19:10:58 +0700

    Create struktur.php

---
+++ profile/struktur.php
@@ -0,0 +1,296 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+        
+    <link rel="icon" type="image/png" sizes="96x96" href="http://www.smkn1-sby.sch.id/assets/template/landing/images/favicon.png">
+	
+    <!-- Google font -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300,400,700,900' rel='stylesheet' type='text/css'>
+    <!-- Css -->
+    <link rel="stylesheet" type="text/css" href="http://www.smkn1-sby.sch.id/assets/template/landing/css/library/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="http://www.smkn1-sby.sch.id/assets/template/landing/css/library/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="http://www.smkn1-sby.sch.id/assets/template/landing/css/library/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="http://www.smkn1-sby.sch.id/assets/template/landing/css/md-font.css">
+    <link rel="stylesheet" type="text/css" href="http://www.smkn1-sby.sch.id/assets/template/landing/css/style.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
+    <style>
+        .slider-text p{
+            color: #fff!important;
+        }
+    </style>
+        <link href="https://unpkg.com/treeflex/dist/css/treeflex.css" rel="stylesheet">
+   
+    <style>
+        .info-acount h3{
+            font-size: 40px; text-align:center;
+        }
+        .info-acount p{
+            text-align: center; font-size:20px;
+        }
+
+        .info-acount ol{
+            color: #666;
+            font-size:20px;
+        }
+
+        .info-acount ul{
+            color: #666;
+            font-size:20px;
+        }
+
+        .kepsek{
+            position: sticky;
+            width: 100px;
+            height: 100px;
+            top: 137px;
+            right: 500px;
+            background-color: #666;
+        }
+    </style>
+
+    <title>SMK Negeri 1 Surabaya</title>
+
+</head>
+<body id="page-top" class="home">
+
+<!-- PAGE WRAP -->
+<div id="page-wrap">
+
+    <!-- PRELOADER -->
+    <div id="preloader">
+        <div class="pre-icon">
+            <div class="pre-item pre-item-1"></div>
+            <div class="pre-item pre-item-2"></div>
+            <div class="pre-item pre-item-3"></div>
+            <div class="pre-item pre-item-4"></div>
+        </div>
+    </div>
+    <!-- END / PRELOADER -->
+
+    <!-- HEADER -->
+    <header id="header" class="header" style="margin-top:1%; margin-bottom:1%;">
+    <div class="container">
+
+        <!-- LOGO -->
+        <div class="logo"><a href="http://www.smkn1-sby.sch.id"><img style="height: 65px; object-fit:cover;" src="http://www.smkn1-sby.sch.id/assets/template/landing/images/logo.png" alt=""></a></div>
+        <!-- END / LOGO -->
+
+        <!-- NAVIGATION -->
+        <nav class="navigation">
+
+            <div class="open-menu">
+                <span class="item item-1"></span>
+                <span class="item item-2"></span>
+                <span class="item item-3"></span>
+            </div>
+
+            <!-- MENU -->
+            
+            <ul class="menu">
+                <li class="menu-item"><a href="http://www.smkn1-sby.sch.id">Home</a></li>
+                <li class="menu-item-has-children active">
+                    <a href="#">Profil</a>
+                    <ul class="sub-menu">
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/profil/visimisi">Visi & Misi</a></li>
+                        <li class=" active"><a href="http://www.smkn1-sby.sch.id/profil/struktur">Struktur Organisasi</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/profil/guru/view">Guru & Tenaga Kependidikan</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/profil/gallery/11/view">Sarana & Prasarana</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/profil/gallery/12/view">Teaching Factory</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/profil/gallery/semua/view">Gallery Foto</a></li>
+                    </ul>
+                </li>
+
+                <li class="menu-item-has-children">
+                    <a href="#">Jurusan</a>
+                    <ul class="sub-menu">
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/jurusan/profil/3" style="line-height:30px;">Akuntansi dan Keuangan Lembaga</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/jurusan/profil/9" style="line-height:30px;">Bisnis Daring dan Pemasaran</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/jurusan/profil/11" style="line-height:30px;">Otomatisasi dan Tata Kelola Perkantoran</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/jurusan/profil/1" style="line-height:30px;">Rekayasa Perangkat Lunak</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/jurusan/profil/4" style="line-height:30px;">Multimedia</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/jurusan/profil/5" style="line-height:30px;">Teknik Komputer dan Jaringan</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/jurusan/profil/6" style="line-height:30px;">Desain Komunikasi Visual</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/jurusan/profil/10" style="line-height:30px;">Produksi Siaran Program Pertelevisian</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/jurusan/profil/8" style="line-height:30px;">Perhotelan</a></li>
+                        
+                    </ul>
+                </li>
+                <li class="menu-item-has-children">
+                    <a href="#">Informasi</a>
+                    <ul class="sub-menu">
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/info/mriso">Mr. Iso</a></li>
+                        <li class=""><a href="http://www.smkn1-sby.sch.id/info/pengumuman/semua">Pengumuman</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item">
+                    <a href="http://www.smkn1-sby.sch.id/berita/semua/list">Berita</a>
+                </li>
+               
+                <!-- 
+                <li style="background-color:lightgreen" class="menu-item-has-children">
+                    <a href="#">PPDB</a>
+                    <ul class="sub-menu">
+                        
+                        <li><a href="http://www.smkn1-sby.sch.id/login">Entri Data Dapodik</a></li>
+                    </ul>
+                </li>
+                -->
+               
+                <li class="menu-item">
+                    <a href="http://www.smkn1-sby.sch.id/modul/semua/view">Modul</a>
+                </li>
+                <li class="menu-item-has-children">
+                    <a href="#">Program</a>
+                    <ul class="sub-menu">
+                        <li><a target="_blank" href="http://lsp.smkn1-sby.sch.id/">Lembaga Sertifikasi Profesi</a></li>
+                        <li><a target="_blank" href="http://bkk.smkn1-sby.sch.id/">Bursa Kerja Kusus (BKK)</a></li>
+                    </ul>
+                </li>
+                                    <li class="menu-item">
+                        <a href="http://www.smkn1-sby.sch.id/login">Log in</a>
+                    </li>
+                            </ul>
+            <!-- END / MENU -->
+            
+        </nav>
+        <!-- END / NAVIGATION -->
+
+    </div>
+</header>    <!-- END / HEADER -->
+    
+        <!-- SUB BANNER -->
+    <section class="sub-banner sub-banner-course">
+        <div class="awe-static bg-sub-banner-course"></div>
+        <div class="container">
+            <div class="sub-banner-content">
+                <h2 class="text-center">SMK NEGERI 1 SURABAYA</h2>
+            </div>
+        </div>
+    </section>
+    <!-- END / SUB BANNER -->
+
+    <section class="profile">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="avatar-acount">  
+                        <div class="info-acount">
+                            <h3 class="md black">Struktur Organisasi</h3>
+                            <div style="text-align: center;">
+                                <img id="bg" src="http://www.smkn1-sby.sch.id/storage/images/struktur.jpg" alt="">
+                            </div>
+                            
+
+                            
+                            <div class="profile-email-address">
+                                
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+            </div>   
+        </div>
+    </section>
+
+    
+    
+    <!-- BEFORE FOOTER -->
+    
+    <!-- END / BEFORE FOOTER -->
+
+    
+    <!-- FOOTER -->
+    <footer id="footer" class="footer">
+    <div class="first-footer">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-md-6">
+                    <div class="widget">
+                        <h3 class="md">Tentang Kami</h3>
+                        <p style="text-align: justify;">
+                            Sekolah Kejuruan di Surabaya, Jawa Timur yang berlokasi di Jl. Smea No. 4, Wonokromo Surabaya, SMK Negeri 1 Surabaya bertekad mencapai perbaikan yang berkesinambungan berdasarkan sistem manajemen mutu ISO 9001:2008.
+                            <br>Telp. 031-8292038 FAX. 031- 8292039, Email : info@smkn1-sby.sch.id
+                        </p>
+                        <ul style="display: inline-flex;">
+                            <li>
+                                <a style="border: 1px solid white; margin:5px; padding:10px; text-align:center;" href="https://www.youtube.com/channel/UCeJCDRP0PhIt7p81pbKR5hg" target="_blank" class="twitter">
+                                    <i style="font-size: 18px;" class="fa fa-youtube-play"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a style="border: 1px solid white; margin:5px; padding:10px; text-align:center;"  href="https://www.instagram.com/smkn1surabaya.official/?hl=id" target="_blank" class="facebook">
+                                    <i style="font-size: 20px;" class="fa fa-instagram" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+
+                
+
+                <div class="col-md-2">
+                    <div class="widget quick_link">
+                        <h3 class="sm">Links Terkait</h3>
+                        <ul class="list-style-block">
+                            <li><a href="http://bkk.smkn1-sby.sch.id/">BKK</a></li>
+                            <li><a href="http://lsp.smkn1-sby.sch.id/">LSP</a></li>
+                            <li><a href="http://www.ppdbjatim.net/">PPDB</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="widget news_letter">
+                        <h3 class="sm">Lokasi SMK Negeri 1 Surabaya</h3>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15829.711567252976!2d112.7338791!3d-7.3057308!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5959ad479fd46d02!2sSMK%20Negeri%201%20Surabaya!5e0!3m2!1sen!2sid!4v1610061759307!5m2!1sen!2sid" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="second-footer">
+        <div class="container">
+            <div class="contact">
+                <div class="email">
+                    <i class="icon md-email"></i>
+                    <a href="#">www.smkn1-sby.sch.id</a>
+                </div>
+                
+                
+            </div>
+            <p class="copyright">Copyright © 2021 SMK Negeri 1 Surabaya. All rights reserved.</p>
+        </div>
+    </div>
+</footer>    <!-- END / FOOTER -->
+
+
+    
+
+
+</div>
+<!-- END / PAGE WRAP -->
+
+<!-- Load jQuery -->
+<script type="text/javascript" src="http://www.smkn1-sby.sch.id/assets/template/landing/js/library/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="http://www.smkn1-sby.sch.id/assets/template/landing/js/library/bootstrap.min.js"></script>
+<script type="text/javascript" src="http://www.smkn1-sby.sch.id/assets/template/landing/js/library/jquery.owl.carousel.js"></script>
+<script type="text/javascript" src="http://www.smkn1-sby.sch.id/assets/template/landing/js/library/jquery.appear.min.js"></script>
+<script type="text/javascript" src="http://www.smkn1-sby.sch.id/assets/template/landing/js/library/perfect-scrollbar.min.js"></script>
+<script type="text/javascript" src="http://www.smkn1-sby.sch.id/assets/template/landing/js/library/jquery.easing.min.js"></script>
+<script type="text/javascript" src="http://www.smkn1-sby.sch.id/assets/template/landing/js/scripts.js"></script>
+<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+</body>
+</html>
